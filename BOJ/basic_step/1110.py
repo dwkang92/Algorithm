c7a928e787c490ee2545950e5fd071c9cf7c6f6e
dwkang92 <dwkang921@gmail.com>
2021-08-20 01:33:11 +0900

    TIL

--- BOJ/basic_step/1110.py
+++ BOJ/basic_step/1110.py
@@ -1,6 +1,10 @@
+# 숫자를 입력받고 반복문에서 사용할 변수 num을 지정합니다.
 input_num = int(input())
-
 num = input_num  # num 변수에 input_num을 지정
+
+# 여기서부터 무한루프 코드를 작성할 것이다. 무한루프 코드를 구성하는 방법은 while True:이다.
+
+# 카운트 되는 숫자는 0부터로 설정하고, while문 상단에서 sum_num과 new_num을 구한다.
 cnt = 0
 while True:
     sum_num = (num // 10) + (num % 10)  # 각 자릿수를 더한수
---
+++ basic grammar/scalar type.py
@@ -0,0 +1,11 @@
+# Python은 4가지의 scala 타입이 있습니다. (정수 int, 실수 float, 값없음 None, 참/거짓의 Boolean)
+# 개인적으로 boolean type에 이해가 부족하였으므로, 이해하기 쉽게 boolean type을 출력해보는 연습을 해보겠습니다.
+
+bool(0)  # False
+bool(1)  # True
+bool(0.0)  # float type을 boolean으로 만들어 보았습니다. 0.0만 False이고 나머지는 True가 됩니다.
+bool(-1.5)  # True
+bool("")  # False. 문자형을 bool형으로 변환하였습니다. empty value만이 false인 것을 확인 할 수 있습니다.
+
+# 코멘트:
+# 파면 팔수록 공부할게 너무 많다...

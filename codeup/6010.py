29b216a1e09d39dd1996be77242103174e16055d
dwkang92 <dwkang921@gmail.com>
2021-08-23 23:39:00 +0900

    Today I learned

--- codeup/6010.py
+++ codeup/6010.py
@@ -1,5 +1,5 @@
 # 변수에 정수값을 저장한 뒤, 정수로 변환하여 출력하기.
 # input()을 사용하여 input()한 값을 가져온다.
-from sys import stdin
-a = int(stdin.readline().rstrip())
+import sys
+a = int(sys.stdin.readline().rstrip())
 print(a)
